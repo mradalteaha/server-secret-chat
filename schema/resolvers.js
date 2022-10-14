06331711fe910d3316c358236f65277b88f674c6
mradalteaha <mrad1999@gmail.com>
2022-10-14 23:00:07 +0300

    adding few to the types

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,17 @@
 //in this file we need to define all the resolvers for the types file 
 import { gql } from "apollo-server-express"; 
 
+const User={
+  displayName: 'display name '  ,
+  email:'email' , 
+  photoURL: 'photo url',
+  uid:'uid', 
+}
 
 const resolvers = {
     Query: {
       hello: () => 'Hello world!',
+      user:(uid)=>User,
     },
   };
 
--- schema/types.js
+++ schema/types.js
@@ -1,18 +1,29 @@
 // in this file we need to define all the types and schema for out objects in the database 
-import { gql } from "apollo-server-express";
+import {
+  gql
+} from "apollo-server-express";
 
-const typeDefs = gql`
+const typeDefs = gql `
 type User{
   displayName: String ,
-        email:String , 
-        photoURL:String , 
-        uid:String ,        
-}
+  email:String , 
+  photoURL:String , 
+  uid:String ,        
+},
+type Room{
+  _id: String ,
+  createdAt:String, 
+  text:String  , 
+  user:MessageUser ,
+},
+type MessageUser{
+  name:String ,
+  avatar:String  , 
+  _id:String  ,
+},
 type Query {
-  hello: String
+  user(uid:String):User,
 }
-
-
 `;
 
 export default typeDefs;
